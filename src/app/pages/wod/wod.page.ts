e9418ddb4462af45d06782b58d28a27739bd20d4
dcmedeiros18 <dc.medeiros@live.com>
2025-06-28 17:14:25 +0100

    Wod Page and route

--- src/app/pages/wod/wod.page.ts
+++ src/app/pages/wod/wod.page.ts
@@ -1,20 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonDatetime, IonModal, IonGrid, IonRow, IonCol, IonIcon, IonButton, IonDatetimeButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonCardSubtitle } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { barbellOutline, home } from 'ionicons/icons';
+import { RouterModule, Router } from '@angular/router';
+
+addIcons({
+  'barbell-outline': barbellOutline, 
+});
 
 @Component({
   selector: 'app-wod',
   templateUrl: './wod.page.html',
   styleUrls: ['./wod.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonCardSubtitle, IonDatetimeButton, IonIcon, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonDatetime, IonModal, IonGrid, IonRow, IonCol, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, RouterModule, Router] 
 })
 export class WodPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+      addIcons({barbellOutline});
+       }
 
   ngOnInit() {
+  }
+
+  goToUserMembership() {   
+    this.router.navigateByUrl('/user-membership'); // caminho certo
   }
 
 }
