756e8bb36aba84ccbca8a7670cf080c3d15d9966
dcmedeiros18 <dc.medeiros@live.com>
2025-07-10 14:09:01 +0100

    page status working

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,25 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private apiUrl = 'http://localhost:3000/auth';
 
-  constructor() { }
-  
-  //login function
-  login(email: string, password: string) {
-    if (email == 'dc.medeiros@live.com' && password == '123456') {
-      return true;
-    }
-    return false;
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, { email, password }).pipe(
+      tap((res: any) => {
+        if (res.token && res.user) {
+          localStorage.setItem('token', res.token);
+          localStorage.setItem('currentUser', JSON.stringify(res.user));
+        }
+      })
+    );
   }
 
-  //password function
-  register(email: string, password: string){
-    if (email == 'dc.medeiros@live.com' && password == '123456' ){
-      return true;
-    }
-    return false;
+  register(email: string, password: string): Observable<any> {
+    return this.http.post(`${this.apiUrl}/register`, { email, password });
   }
 }
