3a6b0a69a059d35ddee895470579041ab7a3ee3c
dcmedeiros18 <dc.medeiros@live.com>
2025-07-12 00:48:45 +0100

    sign up working with save

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar, IonInput, IonItem, IonList, IonInputPasswordToggle, IonButton, IonRippleEffect } from '@ionic/angular/standalone';
+import { ToastController } from '@ionic/angular/standalone';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
@@ -18,7 +19,7 @@ export class RegisterPage implements OnInit {
   email: string = '';
   password: string = '';
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router, private toastController: ToastController) { }
 
   ngOnInit() {
   }
@@ -26,16 +27,33 @@ export class RegisterPage implements OnInit {
   onRegister() {
     const email = this.email;
     const password = this.password;
-    const isRegister = this.authService.login(email, password);
+    const isRegister = this.authService.login(email, password); // ou .register se for cadastro mesmo
+  
     if (isRegister) {
-      // Redirects to the login page after registering
-      this.router.navigate(['/pages/login']);
+      // Exibe algo opcionalmente aqui (ex: Toast ou mensagem)
+      console.log('Successfully registered! Redirecting...');
+  
+      // Aguarda 2 segundos antes de redirecionar
+      this.presentToast('Successfully registered! Redirecting...');
+      setTimeout(() => {
+        this.router.navigate(['/login']);
+      }, 3000);
     }
   }
+  
+  async presentToast(message: string, color: string = 'primary') {
+    const toast = await this.toastController.create({
+      message,
+      duration: 3000,
+      color,
+      position: 'bottom'
+    });
+    await toast.present();
+  }
+  
   goToLogin() {   
     this.router.navigateByUrl('/login'); // caminho certo
   }
-
 }
 
 
